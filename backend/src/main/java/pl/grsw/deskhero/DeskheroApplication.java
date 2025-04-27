e6a8d0c75c65936072115a5e05cb8f3557506b56
Grzegorz Świtalski <gswitalski@gmail.com>
2025-04-27 19:21:40 +0200

    feat: backend and frontend bootstraps

---
+++ backend/src/main/java/pl/grsw/deskhero/DeskheroApplication.java
@@ -0,0 +1,13 @@
+package pl.grsw.deskhero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeskheroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeskheroApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/pl/grsw/deskhero/DeskheroApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.grsw.deskhero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeskheroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
