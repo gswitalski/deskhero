04c96a903ac546885088956d4df77b4595275811
Grzegorz Świtalski <gswitalski@gmail.com>
2025-05-03 18:07:36 +0200

    doc: register user endpoint implenetation plan

---
+++ backend/src/main/java/pl/grsw/deskhero/config/SecurityConfig.java
@@ -0,0 +1,29 @@
+package pl.grsw.deskhero.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                // Wyłącza ochronę CSRF (Cross-Site Request Forgery)
+                // Jest to często robione w przypadku API bezstanowych
+                .csrf(AbstractHttpConfigurer::disable)
+                // Konfiguruje reguły autoryzacji żądań HTTP
+                .authorizeHttpRequests(authorize -> authorize
+                        // Zezwala na wszystkie żądania bez uwierzytelniania
+                        .anyRequest().permitAll()
+                );
+
+        // Buduje i zwraca skonfigurowany łańcuch filtrów bezpieczeństwa
+        return http.build();
+    }
+} 
